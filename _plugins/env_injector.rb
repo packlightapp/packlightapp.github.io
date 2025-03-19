a1417378e925951b6acd91d8b6aa450ad9834383
Mark Oliveira <packlightapp@gmail.com>
2025-03-18 21:11:38 -0400

    fix default cloudkit token for local env

--- _plugins/env_injector.rb
+++ _plugins/env_injector.rb
@@ -1,7 +1,7 @@
 module Jekyll
   class EnvInjector < Generator
     def generate(site)
-      site.config['ck_api_token'] = ENV['CK_API_TOKEN'] || "61ae09ee1a16555deadea5bb183e5803e6d015c4964e870a92fbefd96b6d6a42e"
+      site.config['ck_api_token'] = ENV['CK_API_TOKEN'] || "61ae09ee1a16555deadea5bb183e5803e6d015c4964e870a92fbefd96b6d6a42"
       site.config['ck_env'] = ENV['CK_ENV'] || "development"
     end
   end
